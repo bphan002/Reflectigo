f15d8033c25238d3e8361da9c1fd366593d3a932
Billy <billy.phan4@gmail.com>
2024-11-30 00:40:45 -0800

    env file works

--- app/Hotels.tsx
+++ app/Hotels.tsx
@@ -1,6 +1,13 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { API_URL, GOOGLE_API_KEY } from '@env';
+
+console.log('API URL:', API_URL);  // Outputs: https://api.example.com
+console.log('Google API Key:', GOOGLE_API_KEY);  // Outputs: your-api-key
 
 export default function Hotel() {
+
+  console.log('API URL:', API_URL);  // Outputs: https://api.example.com
+  console.log('Google API Key:', GOOGLE_API_KEY);  // Outputs: your-api-key
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Add hotel or lodging</Text>
